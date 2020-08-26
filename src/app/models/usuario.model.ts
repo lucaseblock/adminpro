9d84e4dd6b799d72946b846c2c08ce6e17b3cac2
lucaseblock <lucasblock049@gmail.com>
2020-08-25 23:15:04 -0300

    Fin sección 15

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -1,3 +1,7 @@
+import { environment } from '../../environments/environment';
+
+const base_url = environment.base_url;
+
 export class Usuario {
 
     constructor(
@@ -9,5 +13,20 @@ export class Usuario {
         public role?: string,
         public uid?: string
     ) {}
+
+    get imagenUrl() {
+
+        if ( this.img.includes('https') ) {
+            return this.img;
+        }
+
+        if ( this.img ) {
+            return `${ base_url }/upload/usuarios/${ this.img }`
+        } else {
+
+            return `${ base_url }/upload/usuarios/no-image`
+        }
+
+    }
     
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarService } from '../../services/sidebar.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { Usuario } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,11 +11,14 @@ import { SidebarService } from '../../services/sidebar.service';
 })
 export class SidebarComponent implements OnInit {
 
+  public usuario: Usuario;
+
   menuItems: any[];
 
-  constructor( private sidebarService: SidebarService ) {
+  constructor( private sidebarService: SidebarService, private usuarioService: UsuarioService ) {
     this.menuItems = sidebarService.menu;
-    console.log(this.menuItems)
+    this.usuario = usuarioService.usuario;
+
   }
 
   ngOnInit(): void {
